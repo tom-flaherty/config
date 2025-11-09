5f65432685b0b43ea25236bf80edaa185b6da697
Tom Flaherty <tom.flaherty03@gmail.com>
2025-11-09 18:02:53 +0000

    add build script

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+parent_working_directory=${PWD##*/}
+pwd_basename=${parent_working_directory:-/}
+if [[ $pwd_basename != "nixconf" ]]; then
+    echo "Error: Not running in nixconf director"
+    exit 1
+fi
+
+echo "sudo nixos-rebuild switch"
+sudo nixos-rebuild switch
+if [[ $? -eq 0 ]]; then
+    echo "Build successful - commiting to git"
+    cp /etc/nixos/configuration.nix ./configuration.nix
+    cp /etc/nixos/hardware-configuration.nix ./hardware-configuration.nix
+    git add ./*nix
+    git commit -m "configuration.nix build"
+    git push
+else
+    echo "Error: Build failed"
+    exit 1
+fi
